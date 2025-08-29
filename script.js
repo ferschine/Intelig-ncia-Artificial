d1c8fba7de7e6ccd1ed14e1b05301543bbb67f48
ferschine <141339296+ferschine@users.noreply.github.com>
2025-08-29 08:41:46 -0300

    .

--- script.js
+++ script.js
@@ -6,6 +6,21 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
+       
+            enunciado: "Ao se vestir para um evento, você opta por quais tons em suas roupas?",
+            alternativas: [
+                {
+                    texto: "Tons de azul, roxo e verde oliva.",
+                    afirmacao: "Você é uma pessoa que se sente bem vestindo a paleta de cores fria,"
+                },
+                {
+                    texto:  "Tons de vermelho, amarelo e verde lima",
+                    afirmacao: "Você é uma pessoa que dá ênfase em priorizar os recursos disponíveis, pensando no meio ambiente e sem descartar o viés econômico."
+                }    
+               
+            ]
+        },
+    {
         enunciado: "Quais cores você costuma usar em suas joias?",
         alternativas: [
             {
@@ -19,21 +34,7 @@ const perguntas = [
            
         ]
     },
-    {
-       
-            enunciado: "As fontes renováveis de energia são aquelas formas de produção de energia em que suas fontes são capazes de manter-se disponíveis durante um longo prazo, contando com recursos que se regeneram ou que se mantêm ativos permanentemente. Com base nisso, temos a energia geotérmica, que consiste em utilizar o calor manifestado em áreas próximas à superfície. Qual a sua opinião sobre esse modelo de energia renovável?",
-            alternativas: [
-                {
-                    texto: "A energia geotérmica precisa ser mais explorada visto que utiliza o calor interno da terra, pois não depende do clima e é inesgotável.",
-                    afirmacao: "Você demonstra um perfil  de pesquisador e inovador que busca por meio da inovação soluções para o meio ambiente e sustentabilidade das gerações posteriores."
-                },
-                {
-                    texto:  "Seria interessante explorar primeiramente fontes renováveis exotérmicas, em segundo plano utilizar a energia geotérmica apesar dos custos elevados de explorações e possibilidades de contaminação de rios. ",
-                    afirmacao: "Você é uma pessoa que dá ênfase em priorizar os recursos disponíveis, pensando no meio ambiente e sem descartar o viés econômico."
-                }    
-               
-            ]
-        },
+
         {
             enunciado: "Hoje em dia é cada vez mais importante conhecer o seu corpo, suas qualidades, suas limitações e entender suas emoções. Saber controlá-las é o desafio das pessoas. A sua auto-estima impacta a sua qualidade de vida?",
 
